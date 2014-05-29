4b41a14b52ff197cee21fec469914a1de64f4f52
emiljas <emiljasinski@gmail.com>
2014-05-29 21:01:12 +0200

    RegexExpressionChecker ParseCharacterGroup in progress

---
+++ MathematicalLinguistics/MathematicalLinguistics.RegularExpression.Tests/RegexExpressionCheckerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace MathematicalLinguistics.RegularExpression.Tests
+{
+    public class RegularExpressionCheckerTests : RegexExpressionChecker
+    {
+        private const string FourCharactersRegex = "abcd";
+        private const string TwoNumbersAddingRegex = "[0-9]+[+][0-9]+";
+
+        [Theory]
+        
+        [InlineData(FourCharactersRegex, "abcd")]
+        
+        //[InlineData(TwoNumbersAddingRegex, "1+2")]
+        public void TestCheck_ValidInput(string regex, string input)
+        {
+            base.Compile(regex);
+
+            bool result = base.Check(input);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        
+        [InlineData(FourCharactersRegex, "abcde")]
+        [InlineData(FourCharactersRegex, "abc")]
+        [InlineData(FourCharactersRegex, "")]
+        public void TestCheck_InvalidInput(string regex, string input)
+        {
+            base.Compile(regex);
+
+            bool result = base.Check(input);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("qwe[4-7]ty", 3, 5, new char[] { '4', '5', '6', '7' })]
+        [InlineData("[a-cA-C]", 0, 8, new char[]{ 'a', 'b', 'c', 'A', 'B', 'C' })] 
+        [InlineData("[xa-bcd]", 0, 8, new char[]{ 'x', 'a', 'b', 'c', 'd' })]
+        public void TestParseCharacterGroup(string regex, int startIndex, int move, char[] characters)
+        {
+            base._regex = regex;
+            var group = base.ParseCharacterGroup(startIndex);
+            Assert.Equal(characters, group.Characters);
+            Assert.Equal(move, group.Move);
+        }
+    }
+}
---
+++ MathematicalLinguistics/MathematicalLinguistics.RegularExpression/RegexExpressionChecker.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathematicalLinguistics.RegularExpression
+{
+    public class State
+    {
+        public State NextState { get; set; }
+        public char[] Characters { get; set; }
+        public bool IsAcceptable { get; set; }
+
+        public bool IsValid(char character)
+        {
+            return Characters.Contains(character);
+        }
+
+        public State GetNextState(char character)
+        {
+            return NextState;
+        }
+    }
+
+    public class CharacterGroup
+    {
+        public char[] Characters { get; set; }
+        public int Move { get; set; }
+    }
+
+    public class RegexExpressionChecker
+    {
+        private State _firstState;
+        private State _currentState;
+
+        protected string _regex;
+        private int _regexIndex;
+
+        public void Compile(string regex)
+        {
+            _regex = regex;
+            State state = null;
+
+            for (_regexIndex = 0; _regexIndex < regex.Length; )
+            {
+                state = new State();
+
+                if (IsCharacterGroup())
+                {
+                    var group = ParseCharacterGroup(_regexIndex);
+                    state.Characters = group.Characters;
+                    _regexIndex += group.Move;
+                }
+                else
+                {
+                    state.Characters = new char[]{ regex[_regexIndex] };
+                    ++_regexIndex;
+                }
+
+                if (_currentState != null)
+                {
+                    _currentState.NextState = state;
+                }
+
+                if (_firstState == null)
+                {
+                    _firstState = state;
+                }
+
+                _currentState = state;
+            }
+
+            if (state != null)
+                state.IsAcceptable = true;
+        }
+
+        public bool Check(string input)
+        {
+            bool isAcceptable = false;
+            char currentCharacter;
+            State currentState = _firstState;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                isAcceptable = false;
+                currentCharacter = input[i];
+
+                if (currentState == null)
+                    return false;
+
+                if(!currentState.IsValid(currentCharacter))
+                    return false;
+                else if (currentState.IsAcceptable)
+                    isAcceptable = true;
+
+                currentState = currentState.GetNextState(currentCharacter);
+            }
+
+            return isAcceptable;
+        }
+
+        private bool IsCharacterGroup()
+        {
+            return _regex[_regexIndex] == '[';
+        }
+
+        protected CharacterGroup ParseCharacterGroup(int startIndex)
+        {
+            int groupLength = _regex.IndexOf(']', startIndex) - startIndex - 1;
+            var group = _regex.Substring(startIndex + 1, groupLength);
+
+            var ranges = group.Replace("-", "").ToCharArray();
+
+            var characters = new List<char>();
+            for (int i = 0; i < ranges.Length; i += 2)
+            {
+                char rangeBegin = ranges[i];
+                char rangeEnd = ranges[i + 1];
+
+                char character = rangeBegin;
+                while (character != rangeEnd)
+                {
+                    characters.Add(character);
+                    ++character;
+                }
+                characters.Add(character);
+            }
+
+            return new CharacterGroup
+            {
+                Characters = characters.ToArray(),
+                Move = groupLength + 2
+            };
+        }
+
+    }
+}
